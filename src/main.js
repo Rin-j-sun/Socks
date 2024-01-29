e862f00eccee67d380cfc8cfab1becacec6a12a5
Rin-j-sun <sabrina.sotnikova@gmail.com>
2024-01-29 14:15:35 +0700

    Добавление описания и фото.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        image: "./src/assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -9,27 +10,30 @@ let app = new Vue({
             {
                 variantId: 2234,
                 variantColor: 'green',
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
                 variantImage: "./src/assets/vmSocks-green-onWhite.jpg", // путь к фотке
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
                 variantImage: "./src/assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-            }
-        },
-
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
 
 
-    }
+
+    },
+    // methods: {
+    //     addToCart() {
+    //         this.cart += 1
+    //     }
+    // },
+    //
+    // updateProduct(variantImage) {
+    //     this.image = variantImage
+    // }
 })
 
 
