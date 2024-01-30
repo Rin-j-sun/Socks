73abc4b458824960435e7ac8ea888bf9fbf2e68c
god <sabrina.sotnikova@gmail.com>
2024-01-30 13:07:08 +0700

    Русификация и исправление ошибок, добавление стилей.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 let eventBus = new Vue();
 
+// Форма отзывовов
 Vue.component('product-review', {
     template: `
-
       <form class="review-form" @submit.prevent="onSubmit">
       <p v-if="errors.length">
         <b>Пожалуйста, исправьте следующие ошибки:</b>
@@ -82,7 +82,7 @@ Vue.component('product-review', {
 
 
 // Вкладки
-
+// страницы, вкладки
 Vue.component('product-tabs', {
     template: `
       <div>
@@ -93,8 +93,8 @@ Vue.component('product-tabs', {
              @click="selectedTab = tab"
        >{{ tab }}</span>
       </ul>
-      <div v-show="selectedTab === 'Reviews'">
-        <p v-if="!reviews.length">There are no reviews yet.</p>
+      <div v-show="selectedTab === 'Отзывы'">
+        <p v-if="!reviews.length">Отзывов о товаре пока нет.</p>
         <ul>
           <li v-for="review in reviews">
             <p>{{ review.name }}</p>
@@ -103,13 +103,13 @@ Vue.component('product-tabs', {
           </li>
         </ul>
       </div>
-      <div v-show="selectedTab === 'Make a Review'">
+      <div v-show="selectedTab === 'Оставить отзыв'">
         <product-review></product-review>
       </div>
-      <div v-show="selectedTab === 'Shipping'">
+      <div v-show="selectedTab === 'Доставка'">
         <shipping-tab :premium="premium"></shipping-tab>
       </div>
-      <div v-show="selectedTab === 'Details'">
+      <div v-show="selectedTab === 'Информация о товаре'">
         <details-tab :details="details"></details-tab>
       </div>
       </div>
@@ -130,14 +130,14 @@ Vue.component('product-tabs', {
     },
     data() {
         return {
-            tabs: ['Reviews', 'Make a Review', 'Shipping', 'Details'],
-            selectedTab: 'Reviews'
+            tabs: ['Отзывы', 'Оставить отзыв', 'Доставка', 'Информация о товаре'],
+            selectedTab: 'Отзывы'
         }
     }
 });
 
 
-
+// Информация о самом продукте/ товаре
 Vue.component('product', {
     props: {
         premium: {
@@ -163,9 +163,9 @@ Vue.component('product', {
 
        <div class="product-info">
            <h1>{{ title }}</h1>
-           <p v-if="inStock">In stock</p>
-           <p v-else>Out of Stock</p>
-          <p>Shipping: {{ shipping }}</p>
+           <p v-if="inStock">В наличии</p>
+           <p v-else>Отсутствует в наличии</p>
+          <p>Доставка: {{ shipping }}</p>
            <div
                    class="color-box"
                    v-for="(variant, index) in variants"
@@ -179,8 +179,11 @@ Vue.component('product', {
                    :disabled="!inStock"
                    :class="{ disabledButton: !inStock }"
            >
-               Add to cart
-           </button>    
+               Добавить в корзину
+           </button>
+
+         <button v-on:click="delitFromCart" class="delitFromCart">Удалить из корзины</button>
+         
        </div>
 
     <product-tabs :reviews="reviews" :premium="premium" :details="details"></product-tabs>
@@ -189,10 +192,10 @@ Vue.component('product', {
  `,
     data() {
         return {
-            product: "Socks",
-            brand: 'Vue Mastery',
+            product: "Носки",
+            brand: 'Абибас',
             selectedVariant: 0,
-            altText: "A pair of socks",
+            altText: "Пара носков",
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
@@ -216,9 +219,10 @@ Vue.component('product', {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
 
-        вудшеFromCart() {
+        delitFromCart() {
             if (this.cart.length > 0) {
-                this.$emit('remove-from-cart', this.cart[this.cart.length - 1]);
+                this.$emit('delit-from-cart', this.cart[this.cart.length - 1]);
+
             }
         },
 
@@ -239,7 +243,7 @@ Vue.component('product', {
         },
         shipping() {
             if (this.premium) {
-                return "Free";
+                return "Бесплатно";
             } else {
                 return 2.99
             }
@@ -247,12 +251,12 @@ Vue.component('product', {
     }
 })
 
-Vue.component('shipping-tab', {
+Vue.component('shipping-tab', {  // Информация о доставке
     template: `
     <div>
-      <p>Shipping Details:</p>
-      <p v-if="premium">Free Shipping</p>
-      <p v-else>Shipping Cost: $2.99</p>
+      <p>Детали доставки :</p>
+      <p v-if="premium">Бесплатная доставка</p>
+      <p v-else>Стоимость доставки : $2.99</p>
     </div>
   `,
     props: {
@@ -263,10 +267,10 @@ Vue.component('shipping-tab', {
     }
 });
 
-Vue.component('details-tab', {
+Vue.component('details-tab', {   // Информаия о товаре
     template: `
     <div>
-      <p>Product Details:</p>
+      <p>Информация о товаре :</p>
       <ul>
         <li v-for="detail in details">{{ detail }}</li>
       </ul>
@@ -288,22 +292,24 @@ let app = new Vue({
         reviews: []
     },
     methods: {
+        // Метод для добавления товара в корзину
         updateCart(id) {
             this.cart.push(id);
-        }
-    },
-
-    delitFromCart(id) {
-            let index = this.cart.indexOf(id);
-            if (index !== -1) {
-                this.cart.splice(index, 1);
-            }
         },
+
+    // Метод для удаления товара из корзины
+    delitFromCart(id) {
+        let index = this.cart.indexOf(id);
+        if (index !== -1) {
+            this.cart.splice(index, 1);
+        }
+    }
+    },
 
     mounted() {
         eventBus.$on('review-submitted', productReview => {
             this.reviews.push(productReview);
         });
-    },
+    }
 
 })
